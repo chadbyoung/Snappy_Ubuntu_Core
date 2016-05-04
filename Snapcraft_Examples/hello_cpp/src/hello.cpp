68b2d1ba6a1d8b4b2b9afb39ff2b1f13ff55c376
chadyoungdell <chad_young@dell.com>
2016-05-04 09:47:02 -0500

    changed name from Snapcraft_Examples to Snapcraft_1.x

--- Snapcraft_Examples/hello_cpp/src/hello.cpp
+++
@@ -1,9 +0,0 @@
-//a simple C++ programe
- #include <iostream>
- using namespace std;
-
- int main()
- {
-    cout << "Hello World! (a c++ program) \n" ;
-    return 0;
- }
--- Snapcraft_Examples/hello_java/src/java_hello_world.java
+++
@@ -1,6 +0,0 @@
-class java_hello_world {
-	public static void main(String args[]){
-		System.out.println("If you are reading this line... the java program worked fine!");
-		}
-	}
-
