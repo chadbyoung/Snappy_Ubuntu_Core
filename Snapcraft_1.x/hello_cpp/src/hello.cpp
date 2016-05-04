e441f579e0ebfe31ab8a8918ac4ccbbf6c9c457a
chadyoungdell <chad_young@dell.com>
2016-05-04 09:43:53 -0500

    Adding Snapcraft 2.x examples

---
+++ Snapcraft_1.x/hello_cpp/src/hello.cpp
@@ -0,0 +1,9 @@
+//a simple C++ programe
+ #include <iostream>
+ using namespace std;
+
+ int main()
+ {
+    cout << "Hello World! (a c++ program) \n" ;
+    return 0;
+ }
---
+++ Snapcraft_1.x/hello_java/src/java_hello_world.java
@@ -0,0 +1,6 @@
+class java_hello_world {
+	public static void main(String args[]){
+		System.out.println("If you are reading this line... the java program worked fine!");
+		}
+	}
+
