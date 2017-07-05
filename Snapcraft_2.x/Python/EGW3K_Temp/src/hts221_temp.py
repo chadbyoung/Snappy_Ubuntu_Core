c04a6600d325b53c27aac8abccf9de278e21362a
Chad Young <chadyoungdell@github.com>
2017-07-05 15:32:58 -0500

    added if/else statement

--- Snapcraft_2.x/Python/EGW3K_Temp/src/hts221_temp.py
+++ Snapcraft_2.x/Python/EGW3K_Temp/src/hts221_temp.py
@@ -22,19 +22,43 @@
 #
 ######################################################################
 
+# This the device you are testing for
+sensor = "hts221"
+
 # The variable below is need for finial formula 
 div1k = 1000
 
+
+# Read the model of the gateway you are working on
+# Read iio:device0
+iio_dev0 = open("/sys/bus/iio/devices/iio:device0/name","r")
+iio_dev0_model = iio_dev0.readline().strip()
+Dev0 = str(iio_dev0_model)
+iio_dev0.close
+
+# Read iio:device1
+iio_dev1 = open("/sys/bus/iio/devices/iio:device1/name","r")
+iio_dev1_model = iio_dev1.readline().strip()
+Dev1 = str(iio_dev1_model)
+iio_dev1.close
+
+# Read iio:device2
+iio_dev2 = open("/sys/bus/iio/devices/iio:device2/name","r")
+iio_dev2_model = iio_dev2.readline().strip()
+Dev2 = str(iio_dev2_model)
+iio_dev2.close
+
+
 # Read the "in_temp_raw" file 
 in_temp_raw = open("/sys/bus/iio/devices/iio:device0/in_temp_raw","r")
-int_raw_input = in_temp_raw.readline()
-InTempRaw = int(int_raw_input)
+flt_raw_input = in_temp_raw.readline()
+InTempRaw = float(flt_raw_input)
 in_temp_raw.close
      
 # Read the "in_temp_offset" file 
 in_temp_offset = open("/sys/bus/iio/devices/iio:device0/in_temp_offset","r")
-int_offset_input = in_temp_offset.readline()
-InTempOffset = int(int_offset_input)
+flt_offset_input = in_temp_offset.readline()
+InTempOffset = float(flt_offset_input)
 in_temp_offset.close
      
 # Read the "in_temp_scale" file 
@@ -55,29 +79,41 @@ in_temp_scale.close
 ######################################################################
 
 def main():
-    def phase1(num1, num2):
-        return num1 + num2
     
-    def phase2 (num1, num2):
-        return num1 * num2
+    #print'DEBUG'
+    #print'Dev0 is', Dev0
+    #print'DEBUG'
     
-    def phase3 (num1, num2):
-        return num1 / num2
+    if Dev0 == "hts221":
+    
+       def phase1(num1, num2):
+           return num1 + num2
+    
+       def phase2 (num1, num2):
+           return num1 * num2
+    
+       def phase3 (num1, num2):
+          return num1 / num2
 
-    # Get the sum of the numbers
-    total1 = phase1(InTempRaw, InTempOffset)
+       # Get the sum of the numbers
+       total1 = phase1(InTempRaw, InTempOffset)
 
-    # Multiply the numbers
-    total2 = phase2(total1, InTempScale)
+       # Multiply the numbers
+       total2 = phase2(total1, InTempScale)
 
-    # Divide by 1000
-    total3 = phase3(total2, div1k)
+       # Divide by 1000
+       total3 = phase3(total2, div1k)
 
-    # Print the temp
-    print ''
-    print 'The internal temperature of the EGW3002 is', format(total3, ',.2f'), 'degrees celsius'
-    print ''
+       # Print the temp
+       print ''
+       print 'The internal temperature of the EGW3002 is', format(total3, ',.2f'), 'degrees celsius'
+       print ''
 
+    else:
+	print'The ST HTS221 is not installed on this machine'
+        print'The device found was a:', Dev0
+        print'The equiv of Dev0 is:', str(iio_dev0_model)
+        print'The device tested against was:', sensor
 
 
 # Call the main function
